59e8ecb51a32da4594372dcdc442a099afd04d2f
Huang Qi <huangqi.0@bytedance.com>
2020-04-14 11:17:15 +0800

    feat(demo): revas timeline

---
+++ demo/revas-timeline/precache-manifest.441ea8bc1644b4fd5b4522ca3cbd2330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9cd13f25d416bb971cb7a424c1946fa",
+    "url": "/demo/revas-pwa/index.html"
+  },
+  {
+    "revision": "961e2f5ebe9c59966dbf",
+    "url": "/demo/revas-pwa/static/css/main.690ef5d6.chunk.css"
+  },
+  {
+    "revision": "79e9bc22b28be1cac665",
+    "url": "/demo/revas-pwa/static/js/2.539db2db.chunk.js"
+  },
+  {
+    "revision": "961e2f5ebe9c59966dbf",
+    "url": "/demo/revas-pwa/static/js/main.a5af7890.chunk.js"
+  },
+  {
+    "revision": "d34891881953f6fa8bcd",
+    "url": "/demo/revas-pwa/static/js/runtime-main.d943534b.js"
+  }
+]);
