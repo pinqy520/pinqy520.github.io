e639ff7b852506a22d56c03409afa11149e7138f
Huang Qi <huangqi.0@bytedance.com>
2020-05-11 15:05:10 +0800

    feat: revas bench for dom compare

---
+++ demo/revas-bench/precache-manifest.d4ba49013a8d155e8da953ef05aa5de4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1908c828a740323775139e790c7dcfb0",
+    "url": "/demo/revas-bench/index.html"
+  },
+  {
+    "revision": "da77fae9d9556dd8f141",
+    "url": "/demo/revas-bench/static/css/main.134c24ad.chunk.css"
+  },
+  {
+    "revision": "363ec618c4cde49a4bf9",
+    "url": "/demo/revas-bench/static/js/2.f1cad380.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/revas-bench/static/js/2.f1cad380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da77fae9d9556dd8f141",
+    "url": "/demo/revas-bench/static/js/main.490e5fef.chunk.js"
+  },
+  {
+    "revision": "64842a7856ef7f45bd38",
+    "url": "/demo/revas-bench/static/js/runtime-main.0a404b6b.js"
+  }
+]);
