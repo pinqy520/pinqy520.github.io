bdb57fecfb8197213fa191074f19399855f815b0
Huang Qi <huangqi.0@bytedance.com>
2020-06-11 23:09:36 +0800

    feat(demo): revas three.js demo

---
+++ demo/revas-three/precache-manifest.3ae3c4b5c590d8831a2266b6548327be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d18ad9a331baae6a1676ac07303a365",
+    "url": "/demo/revas-three/index.html"
+  },
+  {
+    "revision": "2e338e056cd68653b4c2",
+    "url": "/demo/revas-three/static/css/main.21e1a86f.chunk.css"
+  },
+  {
+    "revision": "500ab38e484e3e20cffe",
+    "url": "/demo/revas-three/static/js/2.335c7e88.chunk.js"
+  },
+  {
+    "revision": "61357f828b56381693510b0a4d08b6ea",
+    "url": "/demo/revas-three/static/js/2.335c7e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e338e056cd68653b4c2",
+    "url": "/demo/revas-three/static/js/main.436a5ce5.chunk.js"
+  },
+  {
+    "revision": "f521b875667800a52e67",
+    "url": "/demo/revas-three/static/js/runtime-main.7a1c461c.js"
+  },
+  {
+    "revision": "59fbc93449a296b83262b1b49bb61db0",
+    "url": "/demo/revas-three/static/media/Flamingo.59fbc934.glb"
+  }
+]);
